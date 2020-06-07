1a4feb0a068d12aa69485e6c9f9d492f195bd6f7
Robert HC <bj112143@gmail.com>
2020-06-07 03:09:25 -0500

    Refactor: update version doobie
    - Upgrade version doobie
    - Fixed error in test with version 0.8.8

--- src/main/com/roberthuaman/api/module/shared/infrastructure/persistence/doobie/DoobieDbConnection.scala
+++ src/main/com/roberthuaman/api/module/shared/infrastructure/persistence/doobie/DoobieDbConnection.scala
@@ -1,20 +1,23 @@
 package com.roberthuaman.api.module.shared.infrastructure.persistence.doobie
 
-import cats.effect.IO
+import cats.effect.{Blocker, ContextShift, IO}
 import doobie.Transactor
 import doobie.syntax.ConnectionIOOps
 import doobie.util.transactor.Transactor.Aux
 import com.roberthuaman.api.module.shared.infrastructure.config.DbConfig
+import doobie.util.ExecutionContexts
 
 import scala.concurrent.Future
 
 final class DoobieDbConnection(dbConfig: DbConfig) {
+  implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContexts.synchronous)
 
   val transactor: Aux[IO, Unit] = Transactor.fromDriverManager[IO](
     dbConfig.driver,
     dbConfig.url,
     dbConfig.user,
-    dbConfig.password
+    dbConfig.password,
+    Blocker.liftExecutionContext(ExecutionContexts.synchronous)
   )
 
   def read[T](query: ConnectionIOOps[T]): Future[T] = query.transact(transactor).unsafeToFuture()
--- src/main/com/roberthuaman/api/module/shared/infrastructure/persistence/doobie/TypesConversions.scala
+++ src/main/com/roberthuaman/api/module/shared/infrastructure/persistence/doobie/TypesConversions.scala
@@ -8,7 +8,7 @@ import com.roberthuaman.api.module.video.domain.VideoCategory
 import scala.concurrent.duration._
 
 object TypesConversions {
-  implicit val uuidMeta: Meta[UUID] = Meta[String].xmap(UUID.fromString, _.toString)
-  implicit val durationMeta: Meta[FiniteDuration] = Meta[Long].xmap(_.seconds, _.toSeconds)
-  implicit val videoCategoryMeta: Meta[VideoCategory] = Meta[String].xmap(VideoCategory.apply, _.toString)
+  implicit val uuidMeta: Meta[UUID] = Meta[String].imap(UUID.fromString)(_.toString)
+  implicit val durationMeta: Meta[FiniteDuration] = Meta[Long].imap(_.seconds)(_.toSeconds)
+  implicit val videoCategoryMeta: Meta[VideoCategory] = Meta[String].imap(VideoCategory.apply)(_.toString)
 }
